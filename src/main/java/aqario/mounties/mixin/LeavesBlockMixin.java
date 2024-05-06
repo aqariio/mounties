20b3487b147ef53a05af46ea7bc5a675bb3b03e0
aqariio <aquario.mw@gmail.com>
2024-05-06 18:20:28 -0400

    Make leaves only collide from the top

---
+++ src/main/java/aqario/mounties/mixin/LeavesBlockMixin.java
@@ -0,0 +1,35 @@
+package aqario.mounties.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LeavesBlock.class)
+public abstract class LeavesBlockMixin extends Block implements Waterloggable {
+    public LeavesBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        if (context.isAbove(VoxelShapes.fullCube(), pos, true) && !(world.getBlockState(pos.up()).getBlock() instanceof LeavesBlock)) {
+            return VoxelShapes.fullCube();
+        }
+        return VoxelShapes.empty();
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (entity instanceof LivingEntity && entity.getType() != EntityType.FOX && entity.getType() != EntityType.BEE) {
+            entity.setMovementMultiplier(state, new Vec3d(0.98, 0.95F, 0.98));
+        }
+    }
+}
